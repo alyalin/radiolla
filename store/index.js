e63687444567a0f5512c913fb7734bfa87467cc5
Alexander <occvlt@yandex.ru>
2019-08-24 17:13:35 +0400

    nuxt update to v 2.x

--- store/index.js
+++ store/index.js
@@ -1,55 +1,47 @@
-import Vuex from 'vuex';
+export const state = () => ({
+  playerUrl: null,
+  currentPlaying: null,
+  playlistIsPlaying: false,
+})
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      playerUrl: null,
-      currentPlaying: null,
-      playlistIsPlaying: false,
-    },
+export const mutations = {
+  SET_URL: (state, payload) => {
+    state.playerUrl = payload;
+  },
 
-    mutations: {
-      SET_URL: (state, payload) => {
-        state.playerUrl = payload;
-      },
+  SET_CURRENT_PLAYING: (state, payload) => {
+    state.currentPlaying = payload;
+  },
 
-      SET_CURRENT_PLAYING: (state, payload) => {
-        state.currentPlaying = payload;
-      },
+  SET_IS_PLAYING: (state, payload) => {
+    state.playlistIsPlaying = payload;
+  }
+}
 
-      SET_IS_PLAYING: (state, payload) => {
-        state.playlistIsPlaying = payload;
-      }
-    },
+export const actions = {
+  setPlayerUrl({ commit, state }, payload) {
+    commit('SET_URL', payload);
+  },
 
-    actions: {
-      setPlayerUrl({ commit, state }, payload) {
-        commit('SET_URL', payload);
-      },
+  setCurrentPlaying({ commit, state }, payload) {
+    commit('SET_CURRENT_PLAYING', payload);
+  },
 
-      setCurrentPlaying({ commit, state }, payload) {
-        commit('SET_CURRENT_PLAYING', payload);
-      },
+  setIsPlaying({ commit, state }, payload) {
+    commit('SET_IS_PLAYING', payload);
+  }
+}
 
-      setIsPlaying({ commit, state }, payload) {
-        commit('SET_IS_PLAYING', payload);
-      }
-    },
+export const getters = {
+  getPlayerUrl(state) {
+    return state.playerUrl;
+  },
 
-    getters: {
-      getPlayerUrl(state) {
-        return state.playerUrl;
-      },
+  getCurrentPlaying(state) {
+    return state.currentPlaying;
+  },
 
-      getCurrentPlaying(state) {
-        return state.currentPlaying;
-      },
-
-      getIsPlaying(state) {
-        return state.playlistIsPlaying;
-      }
-    }
-  })
-};
-
-export default createStore
+  getIsPlaying(state) {
+    return state.playlistIsPlaying;
+  }
+}
