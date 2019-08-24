589afb4be4732c7505bee6cf142aa3c93ffd8809
Alexander <occvlt@yandex.ru>
2019-08-24 19:00:27 +0400

    13fa644

---
+++ _nuxt/45f98fa0384ad294f0c3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{199:function(e,t,n){"use strict";n.r(t);var l=n(10),component=Object(l.a)({},function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Select the genre")])},[],!1,null,null,null);t.default=component.exports}}]);
